c7c7a8786f2a51966637ba0af011273d4067f12d
Ruzina Biktasheva <ruzina.3112@gmail.com>
2024-10-01 11:13:27 +0300

    Добавление нового класса GroupHelper

--- fixtures/application_group.py
+++ fixtures/application_group.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixtures.group import GroupHelper
 from fixtures.session import Session_Helper
-
 
 # фикстура для теста добавления группы:
 class ApplicationGroup:
@@ -8,37 +8,12 @@ class ApplicationGroup:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
-        self.session  = Session_Helper(self)
+        self.session = Session_Helper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("https://localhost/addressbook/#")
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-        wd.get("https://localhost/addressbook/group.php")
-
-    def create_new_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_at_list_groups()
-
-    def return_at_list_groups(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
 
     def destroy(self):
         self.wd.quit()
---
+++ fixtures/group.py
@@ -0,0 +1,30 @@
+class GroupHelper():
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+        wd.get("https://localhost/addressbook/group.php")
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_id("content").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_at_list_groups()
+
+    def return_at_list_groups(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
