0fd6dd5bae4a1201c2ebedd76636103c02031404
Ruzina Biktasheva <ruzina.3112@gmail.com>
2024-10-06 15:20:19 +0300

    Оптимизация ввода данных в формы: добавление методов type и type_list

--- fixtures/group.py
+++ fixtures/group.py
@@ -4,20 +4,24 @@ class GroupHelper():
     def __init__(self, app):
         self.app = app
 
+    # проверка передачи параметров при вызове функции (поля для ввода)
+    def type(self, field_name, field_value):
+        wd = self.app.wd
+        if field_value is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(field_value)
+        else:
+            pass
+
     def create(self, group):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
         wd.get("https://localhost/addressbook/group.php")
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.type("group_name", group.name)
+        self.type("group_header", group.header)
+        self.type("group_footer", group.footer)
         wd.find_element_by_name("submit").click()
         self.return_at_home_page()
 
@@ -27,15 +31,9 @@ class GroupHelper():
         wd.get("https://localhost/addressbook/group.php")
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_css_selector('input[name="edit"]').click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.type("group_name", group.name)
+        self.type("group_header", group.header)
+        self.type("group_footer", group.footer)
         wd.find_element_by_name("update").click()
         self.return_at_home_page()
 
