eb7a6f29173088c34f7d9b040ce7c148e9dcb03a
Ruzina Biktasheva <ruzina.3112@gmail.com>
2024-10-23 14:37:07 +0300

    Параметризация тестов для создания групп и контактов

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,16 +1,55 @@
 # -*- coding: utf-8 -*-
 from models.contact import Contact
+import pytest
+import random
+import string
 
-    
-def test_add_contact(app):
+# данные для полей типа строка или textarea
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + ' '*10
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+# данные для телефонов
+def random_phone():
+    return '+7' + ''.join([random.choice(string.digits) for i in range(10)])
+
+# данные для email
+def random_email():
+    return ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(10))]) + '@gmail.com'
+
+# данные для сайта
+def random_site():
+    return ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(10))]) + '.com'
+
+# данные для дат
+def random_date():
+    return str(random.randrange(31))
+
+# данные для месяцев
+def random_month():
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    return months[random.randrange(12)]
+
+# данные для годов
+def random_year():
+    return str(random.randrange(1980, 2000))
+
+
+# тестовые данные
+testdata = [Contact(firstname=random_string("First_name:", 10), middlename=random_string("Middle_name:", 10), lastname=random_string("Last_name:", 10),
+                    nickname=random_string("Nickname:", 10), title=random_string("Title:", 10), company=random_string("Company:", 10),
+                    address=random_string("Address:", 10), home=random_phone(), mobile=random_phone(), work=random_phone(), fax=random_phone(), email=random_email(),
+                    email2=random_email(), email3=random_email(), homepage=random_site(), bday=random_date(), bmonth=random_month(), byear=random_year(), aday=random_date(), amonth=random_month(), ayear=random_year())
+    for i in range(1)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    added_contact = Contact(firstname="First_name", middlename="Middle_name", lastname="Last_name", nickname="Nickname", title="Title", company="Company", address="Address",
-                               home="Telephone_home", mobile="Telephone_mobile", work="Telephone_work", fax="Telephone_fax", email="email",
-                               email2="email_2", email3="email_3", homepage="homepage", bday='1', bmonth='January', byear='2000', aday='2', amonth='February', ayear='2010')
-    app.contact.create(added_contact)
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     # проверка по длине списка
     assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(added_contact)
+    old_contacts.append(contact)
     # проверка по id, имени и фамилии
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 from models.group import Group
+import pytest
+import random
+import string
 
-    
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + ' '*10
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Group(name='', header='', footer='')] + [
+    Group(name=random_string('name:', 10), header=random_string('header:', 20), footer=random_string('footer:', 30))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    added_group = Group(name="name", header="header", footer="footer")
-    app.group.create(added_group)
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     # проверка по длине списка
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(added_group)
+    old_groups.append(group)
     # проверка по id и наименованию
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    added_group = Group(name="", header="", footer="")
-    app.group.create(added_group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(added_group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
