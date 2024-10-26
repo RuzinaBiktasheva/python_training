19a099f2da0a929f6785112ce02aac23e5ebf3b4
Ruzina Biktasheva <ruzina.3112@gmail.com>
2024-10-26 09:44:24 +0300

    Удаление чистки строки (clear) для email

--- tests/test_all_info.py
+++ tests/test_all_info.py
@@ -24,6 +24,5 @@ def merge_phones_like_on_home_page(contact):
 # "склеивание" email
 def merge_emails_like_on_home_page(contact):
     return '\n'.join(filter(lambda x: x != '',
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
